549d64e9f20af61473460ad60d51f87965780d7a
Darsyn <122276971+DE-IGNIS@users.noreply.github.com>
2026-06-20 12:16:21 +0530

    cleanup: removed modal as no longer required

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { BrowserNavProvider } from "./context/BrowserNavContext";
 
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <AppNavigator />
-      </NavigationContainer>
+      <BrowserNavProvider>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </BrowserNavProvider>
     </SafeAreaProvider>
   )
 }
