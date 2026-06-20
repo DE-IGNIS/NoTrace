9fb1accca2df570b1d8b06b0b9bb289083d25755
Darsyn <122276971+DE-IGNIS@users.noreply.github.com>
2026-06-20 11:14:01 +0530

    style: Removed the app logo and bolden the app name

--- mobile/src/navigation/AppNavigator.tsx
+++ mobile/src/navigation/AppNavigator.tsx
@@ -1,34 +1,3 @@
-// import React from 'react';
-// import BottomTabs from './BottomTabs';
-// import Logo from "../../assets/notrace_icon.svg";
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import { View, Text } from 'react-native';
-
-// const Stack = createNativeStackNavigator();
-
-// export default function AppNavigator() {
-//   return (
-//     <Stack.Navigator>
-//       <Stack.Screen
-//         name="NOTRACE"
-//         component={BottomTabs}
-//         options={{
-//           headerShown: true,
-//           headerTitle: () => (
-//             <>
-//               <View>
-//                 <Text>NoTrace</Text>
-//                 <Logo width={120} height={40} />
-//               </View>
-//             </>
-//           ),
-//           headerTitleAlign: "center"
-//         }}
-//       />
-//     </Stack.Navigator>
-//   );
-// }
-
 import React from 'react';
 import BottomTabs from './BottomTabs';
 import Logo from "../../assets/notrace_icon.svg";
@@ -47,8 +16,8 @@ export default function AppNavigator() {
           headerShown: true,
           headerTitle: () => (
             <View style={styles.headerContainer}>
-              <Logo width={28} height={28} />
-              <Text style={styles.headerText}>NoTrace</Text>
+              {/* <Logo width={28} height={28} /> */}
+              <Text style={styles.headerText}>NOTRACE</Text>
             </View>
           ),
           headerTitleAlign: "left"
@@ -62,11 +31,11 @@ const styles = StyleSheet.create({
   headerContainer: {
     flexDirection: "row",
     // alignItems: "center",
-    // justifyContent: "center",
+    justifyContent: "center",
   },
   headerText: {
     marginLeft: 8,
     fontSize: 18,
-    fontWeight: "600",
+    fontWeight: "900",
   },
 });
